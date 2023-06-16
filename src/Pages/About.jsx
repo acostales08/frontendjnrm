5ec5fa6fb90caba3c60622246cd2849f6b5e0244
Your Name <you@example.com>
2023-06-16 13:11:45 +0800

    sample

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div  name='Services' className='bg-gray-500 h-screen w-full '>About</div>
+    <div  name='Services' className='bg-gray-500 h-screen w-full '>About Us</div>
   )
 }
 

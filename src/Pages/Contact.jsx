753a1ad0c3f7192a6dbc04482a50eae54223574f
Marlou T. Vinta <marlouvinta8@gmail.com>
2023-06-16 15:57:50 +0800

    fetch member data

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Contact = () => {
+
+  const [member, setMember] = useState([])
+
+  useEffect(()=>{
+
+    axios.get('http://localhost:8000/api/member').then(res => {
+      console.log(res)
+      setMember(res.data.member)
+    })
+  }, [] )
+
+  var memberdetails = "";
+   memberdetails = member.map(({index, fullname, email, username, password}) => {
+    return(
+      <tr key={index}>
+        <td>{fullname}</td>
+        <td>{email}</td>
+        <td>{username}</td>
+        <td>{password}</td>
+      </tr>
+    )
+  })
+
   return (
-    <div  name='Services' className='bg-gray-700 h-screen w-full '>Contact</div>
+    <div  name='Services' className='bg-gray-700 h-screen w-full '>
+      <table>
+        <thead>
+          <tr>
+            <th>fullname</th>
+            <th>email</th>
+            <th>username</th>
+            <th>password</th>
+          </tr>
+          <tbody>
+            {memberdetails}
+          </tbody>
+        </thead>
+      </table>
+    </div>
   )
 }
 
